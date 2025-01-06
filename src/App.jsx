743d9849e9d6abab1bfa7e41a08663f8457c8d8c
Ben Read <ben@mintcreative.com>
2025-01-06 14:35:08 +0000

    remove unneeded react fragments from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,13 @@ import Board from "./components/Board";
 
 function App() {
   return (
-    <>
-      <div className="bg-green-600 flex min-h-screen p-5">
-        <div className="bg-green-200 w-full max-w-[1144px] border-4 border-black rounded-2xl mx-auto flex flex-col">
-          <Header />
-          <Board />
-          <Footer />
-        </div>
+    <div className="bg-green-600 flex min-h-screen p-5">
+      <div className="bg-green-200 w-full max-w-[1144px] border-4 border-black rounded-2xl mx-auto flex flex-col">
+        <Header />
+        <Board />
+        <Footer />
       </div>
-    </>
+    </div>
   );
 }
 
