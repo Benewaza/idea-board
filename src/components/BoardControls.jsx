7a49f6a818a46e10e3b57a7e2a5e59f28e209c4e
Ben Read <ben@mintcreative.com>
2025-01-06 14:41:45 +0000

    accesibility improvements for Boardcontrols

--- src/components/BoardControls.jsx
+++ src/components/BoardControls.jsx
@@ -1,14 +1,19 @@
 const BoardControls = ({ emitModalVisibility, emitSortItems }) => (
   <div className="mb-4 flex flex-col sm:flex-row">
     <button
+      type="button"
       onClick={() => emitModalVisibility(true)}
       className="border-2 px-4 py-2 bg-gray-200 hover:bg-gray-300 border-black font-bold text-sm mb-3 sm:mb-0"
     >
       + New Idea
     </button>
     <div className="sm:ml-auto">
-      <label className="text-xs font-semibold">Filter by: </label>
+      <label className="text-xs font-semibold" htmlFor="filter">
+        Filter by:{" "}
+      </label>
       <select
+        name="filter"
+        id="filter"
         onChange={(e) => emitSortItems(e)}
         className="w-36 px-2 py-2 border-2 border-black rounded-none outline-0 font-bold text-sm text-black bg-gray-200"
       >
